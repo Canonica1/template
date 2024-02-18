73bd6f5bd96d73a644d9fea3a9e8d62199a38b55
Владислав Романовский <88035227+Canonica1@users.noreply.github.com>
2024-02-18 09:10:00 +0300

    Update sdya.cpp

--- sdya.cpp
+++ sdya.cpp
@@ -330,3 +330,16 @@ while True:
         print("stupid: ")
         print(open("st.out").read(), end="")
         break
+
+int gcd(int a, int b, int &x, int &y) {
+    if (a == 0) {
+        x = 0;
+        y = 1;
+        return b;
+    }
+    int x1, y1;
+    int d = gcd(b % a, a, x1, y1);
+    x = y1 - (b / a) * x1;
+    y = x1;
+    return d;
+}
